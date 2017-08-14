7ba82a4edaf218a76720c1b2fa78ccc3536cadfd
ESTUDIANTE <ESTUDIANTE@S4-21>
2017-08-14 06:35:03 -0500

    Programa de Hola Mundo, jajajaja.

---
+++ Prueba 1/src/prueba/pkg1/Prueba1.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba.pkg1;
+
+/**
+ *
+ * @author ESTUDIANTE
+ */
+public class Prueba1 {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+    
+}
